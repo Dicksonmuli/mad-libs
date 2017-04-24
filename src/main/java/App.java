7234aab96a3af26d0d3f25155a26ef5d1b39b714
dicksonmuli <dicksonmully6@gmail.com>
2017-04-24 19:05:10 +0300

    story file- creating dynamic data in templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,19 +8,19 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-		String layout = "templates/layout.vtl";
+		String layout = "templates/layout.vtl";//layout
 //get() indicates the area of application to be accessed via HTTP GET request
 	 get("/", //index page
 	 (request, response) -> {
 		 Map<String, Object> model = new HashMap<String, Object>();
-		 model.put("template", "templates/form.vtl");
+		 model.put("template", "templates/form.vtl");//file path
 		 return new ModelAndView(model, layout);
 	 }, new VelocityTemplateEngine());
 //get() indicates the area of application to be accessed via HTTP GET request
 	 get("/story", //matching the action on form
 	 (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/story.vtl");
+      Map<String, Object> model = new HashMap<String, Object>();//defining instance of HashMap
+      model.put("template","templates/story.vtl"); //file path
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
