570f3589b966d472458640973228c5ef2ae6bfe1
dicksonmuli <dicksonmully6@gmail.com>
2017-04-24 18:56:23 +0300

    defining another route (story)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,11 +9,19 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
 		String layout = "templates/layout.vtl";
-
-	 get("/", (request, response) -> {
+//get() indicates the area of application to be accessed via HTTP GET request
+	 get("/", //index page
+	 (request, response) -> {
 		 Map<String, Object> model = new HashMap<String, Object>();
 		 model.put("template", "templates/form.vtl");
 		 return new ModelAndView(model, layout);
 	 }, new VelocityTemplateEngine());
+//get() indicates the area of application to be accessed via HTTP GET request
+	 get("/story", //matching the action on form
+	 (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/story.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
